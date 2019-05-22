03d38cdeebe7d362397b4a03e4c82e6d4d572309
Jon Seaton <jonathanseaton29@gmail.com>
2019-05-21 19:46:33 -0600

    Adding files and styling

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hadassah Beard',
+        username: '',
+        email: 'kildjean@att.net'
+    }, 
+    {
+        id: 2,
+        name: 'Callie Gregory',
+        username: '',
+        email: 'jnolan@msn.com'
+    },
+    {
+        id: 3,
+        name: 'Bertram Lynch',
+        username: '',
+        email: 'helger@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Juan Foster',
+        username: '',
+        email: 'msherr@me.com'
+    },
+    {
+        id: 5,
+        name: 'Khalil Knapp',
+        username: '',
+        email: 'webinc@live.com'
+    },
+    {
+        id: 6,
+        name: 'Sabina Francis',
+        username: '',
+        email: 'timlinux@att.net'
+    },
+    {
+        id: 7,
+        name: 'Clyde Puckett',
+        username: '',
+        email: 'rmcfarla@outlook.com'
+    },
+    {
+        id: 8,
+        name: 'Faiz Crawford',
+        username: '',
+        email: 'seanq@aol.com'
+    },
+    {
+        id: 9,
+        name: 'Kayson Hancock',
+        username: '',
+        email: 'pavel@hotmail.com'
+    },
+    {
+        id: 10,
+        name: 'Haseeb Burn',
+        username: '',
+        email: 'ingolfke@sbcglobal.net'
+    },
+
+]
