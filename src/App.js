0fbfaeccbdcd9b151e83a663f89c8bcffae9362c
Jon Seaton <jonathanseaton29@gmail.com>
2019-05-24 11:31:32 -0600

    adding styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
-import SearchBox from './SearchBox';
+import SearchBox from "./SearchBox";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
