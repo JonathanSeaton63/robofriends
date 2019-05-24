3b5d11b27d5db7f1b9ee474e6c1b014f9bbf8d05
Jon Seaton <jonathanseaton29@gmail.com>
2019-05-24 15:14:42 -0600

    final deployed version

---
+++ build/precache-manifest.d070d0fd73c41e53b5dfdb612ccbc734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83e8ee6c8e3e2f9c17b8f4fa62287ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5ec356a43f50fa7e0dae",
+    "url": "/static/css/main.d65cabb1.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "5ec356a43f50fa7e0dae",
+    "url": "/static/js/main.b2cc9ef9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
